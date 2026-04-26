a8b422db9587b2e08f172b2a45573423053ba2f3
patorres2 <torres_pame@hotmail.com>
2026-04-26 16:06:59 -0300

    Se agregan credenciales de eliseo

--- src/Login.tsx
+++ src/Login.tsx
@@ -11,7 +11,7 @@ const authorizedUsers = [
   { email: 'torrespame2@gmail.com', password: 'taller2026' },      // Cédula del alumno 1
   { email: 'dariocalvo89@gmail.com', password: 'tulio2026' },      // Cédula del alumno 2
   { email: 'selenitus27@gmail.com', password: 'nuruoli2026' },      // Cédula del alumno 3
-  { email: 'alumno4@ejemplo.com', password: '44455566' },      // Cédula del alumno 4
+  { email: 'eliseoov@gmail.com', password: 'eliseo2026' },      // Cédula del alumno 4
   { email: 'alumno5@ejemplo.com', password: '77788899' },      // Cédula del alumno 5
   { email: 'alumno6@ejemplo.com', password: '12398745' },      // Cédula del alumno 6
   { email: 'alumno7@ejemplo.com', password: '32165487' },      // Cédula del alumno 7
